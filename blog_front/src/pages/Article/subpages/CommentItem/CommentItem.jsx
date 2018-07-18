591054ba1b144b7ee2326d98ab4d3e1e329b6191
tuwq <aheadqiang@126.com>
2018-07-18 17:35:39 +0800

    子评论模态框,添加评论redux

--- blog_front/src/pages/Article/subpages/CommentItem/CommentItem.jsx
+++ blog_front/src/pages/Article/subpages/CommentItem/CommentItem.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin'
 import { withRouter } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import * as commentActions from 'store/actions/comment' 
 
 import './CommentItem.less'
 import './MCommentItem.less'
@@ -10,10 +13,19 @@ class CommentItem extends React.Component {
 	constructor(props,context) {
 		super(props,context)
 		this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this)
+		this.childModal = this.childModal.bind(this)
 	}
 
 	componentDidMount() {
 		
+	}
+
+	childModal(id) {
+		if (this.props.isModal) {return}
+		this.props.commentActions.load({
+			modalStatus: true,
+			cid: id
+		})
 	}
 
 	render() {	
@@ -33,7 +45,7 @@ class CommentItem extends React.Component {
 						</span>
 					</div>
 					<div className="reply">
-						<button>回复</button>
+						<button onClick={this.childModal.bind(this,1)}>回复</button>
 					</div>
 				</div>
         	</div>
@@ -41,6 +53,20 @@ class CommentItem extends React.Component {
 	}
 }
 
-export default withRouter(CommentItem)
+function mapStateToProps(state) {
+    return {
+     // state.modal 对应的reducer注册时的名称
+        comment: state.comment
+    }
+}
+function mapDispatchToProps(dispatch) {
+    return {
+        commentActions: bindActionCreators(commentActions, dispatch)
+    }
+}
+
+export default withRouter(
+	connect(mapStateToProps, mapDispatchToProps)(CommentItem)
+)
 
 
